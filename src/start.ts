cabc97d78d443efacbef2c50a9c6d3ed254914b0
Douglas Blumeyer <douglas.blumeyer@gmail.com>
2019-04-09 12:51:11 -0700

    1.0.20: constant case integers

--- src/start.ts
+++ src/start.ts
@@ -1,4 +1,4 @@
-// import { pattern } from '@musical-patterns/pattern-performer-qa'
+import { pattern } from '@musical-patterns/pattern-performer-qa'
 import { to } from '@musical-patterns/utilities'
 import {
     enableImmersiveAudio,
@@ -12,7 +12,7 @@ import {
 
 const setupQa: () => Promise<void> =
     async (): Promise<void> => {
-        // await setupPerformer({ pattern })
+        await setupPerformer({ pattern })
         const { enterImmersiveAudio, exitImmersiveAudio }: ToggleImmersiveAudioHandlers = enableImmersiveAudio()
 
         const setTimeButton: HTMLElement = document.createElement('button')
