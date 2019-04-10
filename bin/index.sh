e2f90990c4eee91b06ef23e65224a1d3f3ab6cd9
Douglas Blumeyer <douglas.blumeyer@gmail.com>
2019-04-10 09:23:40 -0700

    1.0.24: we will get this right eventually

--- bin/index.sh
+++ bin/index.sh
@@ -4,5 +4,5 @@ set -e
 
 tsc -p tsconfig.node.json
 if [[ $? == 0 ]] ; then
-	NODE_ENV=snapshots ts-node -P tsconfig.node.json node_modules/@musical-patterns/snapshot/bin/snapshots.js
+	NODE_ENV=snapshots ts-node -P tsconfig.node.json node_modules/@musical-patterns/material/bin/snapshots.js
 fi
