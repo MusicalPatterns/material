8bb4bd7ef44f684ccc9fbe8b1c8c875916a1450b
Douglas Blumeyer <douglas.blumeyer@gmail.com>
2019-04-26 19:41:42 -0700

    1.0.75: for materialqa

--- src/performer/preparation/oscillator/constants.ts
+++ src/performer/preparation/oscillator/constants.ts
@@ -1,9 +1,9 @@
 // tslint:disable no-magic-numbers
 
-import { as, Gain, UnitScalar } from '@musical-patterns/utilities'
+import { as, Gain, NormalScalar } from '@musical-patterns/utilities'
 
-const GAIN_SCALAR_FOR_WEB_AUDIO_OSCILLATORS: UnitScalar<Gain> =
-    as.UnitScalar<Gain>(0.1)
+const GAIN_SCALAR_FOR_WEB_AUDIO_OSCILLATORS: NormalScalar<Gain> =
+    as.NormalScalar<Gain>(0.1)
 
 export {
     GAIN_SCALAR_FOR_WEB_AUDIO_OSCILLATORS,
