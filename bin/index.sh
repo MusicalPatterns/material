157750731c2c46c02f56eac8e93e77353c371ef4
Douglas Blumeyer <douglas.blumeyer@gmail.com>
2020-03-01 09:49:02 -0800

    1.0.118: midi, try II

--- bin/index.sh
+++ bin/index.sh
@@ -2,12 +2,14 @@
 
 set -e
 
-MATERIAL_DIR="./node_modules/@musical-patterns/material"
+MATERIAL_DIR="./node_modules/@musical-patterns/material/"
+
+CMD="$1"
 
 tsc -p tsconfig.node.json
 
-if [[ -f ${MATERIAL_DIR}bin/${CMD}.sh ]] ; then
-  NODE_ENV=material ts-node -P tsconfig.node.json ${MATERIAL_DIR}/bin/${CMD}.js
+if [[ -f ${MATERIAL_DIR}bin/${CMD}.js ]] ; then
+	NODE_ENV=material ts-node -P tsconfig.node.json ${MATERIAL_DIR}/bin${CMD}.js
 else
 	echo "'musical-patterns-material ${CMD}' is not a defined Material command. Or you are using a musical-patterns-material from a foreign PATH and it is not actually installed in your current node_modules. Try running 'which musical-patterns-material' to see what's up."
 fi
