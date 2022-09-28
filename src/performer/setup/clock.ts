abe2679925c66a48992931c3f6ab0b3cf3a4cc7b
Douglas Blumeyer <douglas.blumeyer@gmail.com>
2022-09-28 12:28:05 -0700

    1.0.130: attempt to inline the worker did not work... back to worker-loader

--- src/performer/setup/clock.ts
+++ src/performer/setup/clock.ts
@@ -1,6 +1,7 @@
 import { Maybe } from '@musical-patterns/utilities'
 import { StateKey, store } from '../state'
 import { onClockMessage } from './onClockMessage'
+import Clock from './clock.worker'
 
 const setupClock: () => Promise<void> =
     async (): Promise<void> => {
@@ -9,8 +10,9 @@ const setupClock: () => Promise<void> =
         if (oldClock) {
             oldClock.terminate()
         }
-        
-        const clock: Worker = new Worker(new URL('./clock.worker?inline', import.meta.url))
+
+        // @ts-ignore
+        const clock: Worker = new Clock()
         clock.onmessage = onClockMessage
 
         store.dispatch({ type: StateKey.CLOCK, data: clock })
